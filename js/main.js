0c01f778584ada60555e7b61d9568ca4ff9a5dfc
Caleb Parrra <mccalebyt@gmail.com>
2022-02-09 19:23:55 -0500

    logica terminada

--- js/main.js
+++ js/main.js
@@ -1,50 +1,266 @@
+let palabras = Array("ALURA", "CHALLENGE", "PROGRAMACION","JAVASCRIPT", "RETO");
 
-const PALABRAS = [
-    "ALURA",
-    "CHALLENGE",
-    "ORACLE",
-    "JAVASCRIPT",
-    "PROGRAMACION",
-    "CSS",
-    "HTML"
-]
+let palabraOculta = ""; //la palabra random que se escoge del array
+let palabraAdivinando = "" //palabra que va adivinando el user
+let vidas = 4;
+document.getElementById("boton").addEventListener("click", comprobar);
 
-var vidas = 7;
-var fallos = 0;
+iniciar();
+//logica del juego
+function iniciar(){
+    palabraOculta = palabras[Math.floor(Math.random()*palabras.length)];
+    // console.log(palabraOculta);
 
-//agregar palabras al array y elegir palabra random de el
-let elegirPalabraRandom = () =>{
-    var palabrasInsertar = document.getElementById("palabras__insertar");
-    // console.log(palabrasInsertar);
-    var palabraInsertada = palabrasInsertar.value;
-    PALABRAS.push(palabraInsertada);
-    console.log(PALABRAS);
+    for(let i=0; i < palabraOculta.length; i++){
+        palabraAdivinando+="_ ";
+    }
+    document.getElementById("frase").innerHTML=palabraAdivinando;
+}
 
-    //elegir palabra random del array
-    var Random = Math.floor(Math.random()*PALABRAS.length);
-    var palabraRandom = PALABRAS[Random];
-    palabrasInsertar.value = "";
-    console.log(`la palabra random es: ${palabraRandom}`);
 
-    //escribimos la palabra random con guiones
-    convertirPalabraAguiones(palabraRandom);
+
+
+function comprobar(){
+    let letra = document.getElementById("letra").value.toUpperCase();
+    // palabraOculta = palabraOculta.toUpperCase();
+    let nuevo = "";
+
+    for (let i = 0; i<palabraOculta.length; i++){
+        if (letra == palabraOculta[i]){
+            nuevo = nuevo + letra + " ";
+        }else{
+            nuevo = nuevo + palabraAdivinando[i*2] + " ";
+        }
+    }
+    if (nuevo == palabraAdivinando){
+        vidas--;
+        document.getElementById("vida").innerHTML=`Te quedan ${vidas} vidas`
+    }
+    palabraAdivinando = nuevo;
+    document.getElementById("frase").innerHTML = palabraAdivinando;
+
+    if (vidas == 0){
+        alert("te quedaste sin vidas :((");
+    }
+    if(palabraAdivinando.search("_")== -1){
+        alert("ganaste :p");
+    }
+    document.getElementById("letra").value="";
+    document.getElementById("letra").focus();
+
+    dibujar();
+}
+
+function dibujar(){
+    var canvas = document.getElementById("lienzo");
+    if(canvas.getContext){
+        var ctx = canvas.getContext("2d");
+        //dibuja la base de la horca
+        ctx.beginPath();
+        ctx.moveTo(30, 200);
+        ctx.lineTo(30, 10);
+        ctx.lineTo(150, 10);
+        ctx.lineTo(150, 20);
+        ctx.stroke();
+
+        if(vidas <=3){
+            ctx.beginPath();
+            ctx.arc(150, 40, 20, 0, Math.PI*2);
+            ctx.stroke();
+        }
+
+        if(vidas<=2){
+            ctx.beginPath();
+            ctx.moveTo(150, 60);
+            ctx.lineTo(150, 100);
+            ctx.stroke();
+        }
+
+        if(vidas<=1){
+            ctx.beginPath();
+            ctx.moveTo(150, 60);
+            ctx.lineTo(130, 100);
+            ctx.stroke();
     
-}
-var pushPalabraArray = document.getElementById("palabra__enviar"); //boton para enviar palabras al array
-pushPalabraArray.onclick = elegirPalabraRandom;
+            ctx.beginPath();
+            ctx.moveTo(150, 60);
+            ctx.lineTo(170, 100);
+            ctx.stroke();
+        }
+
+        if(vidas==0){
+            ctx.beginPath();
+            ctx.moveTo(150, 100);
+            ctx.lineTo(170, 130);
+            ctx.stroke();
+    
+            ctx.beginPath();
+            ctx.moveTo(150, 100);
+            ctx.lineTo(130, 130);
+            ctx.stroke();
+        }
+
+    }
+};
 
 
-//tomamos la letra ingresada y 
-var letraIngresada = () =>{
-    var letra = document.getElementById("campo__letra").value;
-    console.log(`la letra ingresada es: ${letra}`);
-}
-var btnProbarLetra = document.getElementById("probar__letra");
-btnProbarLetra.onclick = letraIngresada;
 
-//convertir la palabra random a guiones
-var convertirPalabraAguiones = (palabraRandom) =>{
-    var palabraConGuiones = palabraRandom.replace(/./g, "_ ");
-   
-    console.log(palabraConGuiones);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
